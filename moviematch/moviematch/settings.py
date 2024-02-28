3aa2bf848a24c744c31f180aafccb35d5d306814
Sergio Navarrete <sergionvte11@gmail.com>
2024-02-27 19:25:48 -0600

    Agregada la variable NPM_BIN_PATH.

--- moviematch/moviematch/settings.py
+++ moviematch/moviematch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import platform
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# NPM for Windows
+system = platform.system()
+if system == 'Windows':
+    NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
