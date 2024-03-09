1fb810a066808fac914ab8f2347c45e3dd10169b
Sergio Navarrete <sergionvte11@gmail.com>
2024-03-09 15:28:53 -0600

    Agregada la pagina principal. Cambios en los colores de tailwind. Agregados logos e imagenes.

--- moviematch/theme/static_src/tailwind.config.js
+++ moviematch/theme/static_src/tailwind.config.js
@@ -42,7 +42,25 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'bg0_h': '#1d2021',
+                'bg0': '#282828',
+                'bg1': '#3c3836',
+                'bg2': '#504945',
+                'bg3': '#665c54',
+                'bg4': '#7c6f64',
+                'gray': '#928374',
+                'orange': '#d65d0e',
+                'bg0_s': '#32302f',
+                'fg4': '#a89984',
+                'fg3': '#bdae93',
+                'fg2': '#d5c4a1',
+                'fg1': '#ebdbb2',
+                'fg0': '#fbf1c7',
+                'orange_slow': '#fe8019'
+            }
+        },
     },
     plugins: [
         /**
